8c538d13a87db8e81ffbad7646ec355104e499c6
Vinay Kushwah <108466228+vinay8962@users.noreply.github.com>
2023-03-31 14:44:10 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+var box = document.getElementById('box');
+box.style.top = Math.floor(Math.random() * 50).toString() + "px";
+box.style.left = Math.floor(Math.random() * 50).toString() + "px";
+
+box.addEventListener('mousemove', function(){
+    let viewport_width=window.innerWidth;
+    let viewport_height=window.innerHeight;
+
+    let box_height=box.clientHeight;
+    let box_width=box.clientWidth;
+
+    box.style.top = Math.floor(Math.random() * (viewport_height-box_height)).toString() + "px";
+    box.style.left  = Math.floor(Math.random() * (viewport_width-box_width)).toString() + "px";
+})
